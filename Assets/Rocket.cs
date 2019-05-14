e770bcb6696d01c7678188df722365f563a35f39
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-05-13 22:05:29 -0700

    Added rocket thrust movement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Can thrust while rotating
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Add force in direction ship is facing
+            rigidBody.AddRelativeForce(Vector3.up);
+        } 
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
+}
